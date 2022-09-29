1fd534c11131e9b582a99aecbff4a0fde7d3d629
INVIM19 <INVIM19@IN-L-7401871.asiapacific.abb.com>
2022-09-29 12:13:50 +0530

    update

---
+++ JavaSelenium/src/day2/WD009_LocatingDynamicLink.java
@@ -0,0 +1,45 @@
+package day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import lib.AUT_Lib;
+
+public class WD009_LocatingDynamicLink {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = AUT_Lib.initAUTBrowser("Edge");
+		if(driver!=null)
+		{
+			driver.navigate().to("https://www.amazon.in/ref=nav_logo");
+			
+			if(driver.getTitle().contains("Online"))
+			{
+				System.out.println("Navigated to the AUT Website");
+				
+				driver.findElement(By.linkText("Electronics")).click();
+				AUT_Lib.pauseSeconds(2);
+				driver.navigate().back();
+				AUT_Lib.pauseSeconds(6);
+				driver.findElement(By.linkText("Books")).click();
+				AUT_Lib.pauseSeconds(2);
+				
+			}
+			else
+			{
+				System.err.println("Navigated to invalid website");
+			}
+			
+			driver.quit();
+		}
+		else
+		{
+			System.err.println("Webdriver Initialization failed and execution stopped.");
+		}
+				
+		AUT_Lib.pauseSeconds(4);
+		driver.quit();
+	}
+
+}
